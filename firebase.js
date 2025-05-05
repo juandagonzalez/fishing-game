44443c0ff6570ec1f883782716a3fdbdee3c39ad
David-Gonzalez-H <david@onesafe.io>
2025-05-04 20:23:32 -0500

    Ignore .env

--- firebase.js
+++ firebase.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.ts (or wherever you initialize Firebase)
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDixifVgoDuJ-LcC-zjGrT1hIGlEKqgCF0",
-  authDomain: "fishing-game-27619.firebaseapp.com",
-  projectId: "fishing-game-27619",
-  storageBucket: "fishing-game-27619.firebasestorage.app",
-  messagingSenderId: "369922583873",
-  appId: "1:369922583873:web:a68e015784129007d505c0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export default app;
